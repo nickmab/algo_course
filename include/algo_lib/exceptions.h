633b26b20d5ba626d529ec5d459f6a572c279d66
Nick Mabbutt <nickmab@gmail.com>
2020-01-14 20:37:23 +1100

    Removed pointless intermediary exception subclass

--- include/algo_lib/exceptions.h
+++ include/algo_lib/exceptions.h
@@ -5,21 +5,16 @@
 
 namespace mabz {
 
-class CustomException : public std::exception
-{
-private:
-	std::string mMessage;
-
-public:
-	CustomException(const std::string& msg) : std::exception(), mMessage(msg) {}
-	virtual const char* what() const override { return mMessage.c_str(); }
-};
-
 #define CUSTOM(name) \
-	struct name : public CustomException \
-	{ \
-		name(const std::string& msg) : CustomException(msg) {} \
-	};
+class name : public std::exception \
+{ \
+private: \
+	std::string mMessage; \
+\
+public: \
+	name(const std::string& msg) : std::exception(), mMessage(msg) {} \
+	virtual const char* what() const override { return mMessage.c_str(); } \
+};
 
 CUSTOM(IndexOutOfRange)
 CUSTOM(IllegalArgumentException)
